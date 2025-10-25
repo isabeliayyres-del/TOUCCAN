4ad4e4c7a1017571ea2fca8a3fd575e10c522b6c
thaisaguiarv <thataguiar61@github.com>
2025-10-25 18:07:18 -0300

    Deixando o prdojeto menos "verboso" retirando arquivos desnecessarios

--- touccan_backend/settings.py
+++ touccan_backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "touccan_backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "loja_virtual",
+        "USER": "thais",
+        "PASSWORD": "Senha@123",
+        "HOST": "localhost",
+        "PORT": "3306"
     }
 }
 
