1fca6462ceb9685cd50525c842884f130340fbf2
thaisaguiarv <thataguiar61@github.com>
2025-10-30 23:21:12 -0300

    removendo arquivos deletados

--- touccan_backend/transacoes/Transações.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("pedido/criar/<int:produto_id>/", views.criar_pedido, name="criar_pedido"),
-    path("pedido/<int:pedido_id>/", views.detalhes_pedido, name="detalhes_pedido"),
-    path("pedido/<int:pedido_id>/pagar/", views.pagar_pedido, name="pagar_pedido"),
-]
